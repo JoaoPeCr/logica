fd38142b1838dffcb18ebd9ac92245fc48ad5383
João P. Corrêa <154474733+JoaoPeCr@users.noreply.github.com>
2024-07-29 18:53:39 -0300

    .

--- meus_codigos/cap.4/nodejs/ex4_9.js
+++ meus_codigos/cap.4/nodejs/ex4_9.js
@@ -1,4 +1,10 @@
-const prompt = require("prompt-sync")();
-const centena = prompt("Número (centena): ");
+const prompt = require("prompt-sync")(); //adiciona o pacote
+const numero = Number(prompt("Número (centena): ")); //recebe a entrada
 
-const 
+if (numero < 100 || numero >= 1000 ) {      //diz até onde vai o programa
+  num1 = Math.floor(numero / 100)           //pega a unidade da centena (001)
+  num2 = Math.floor((numero % 100) / 10)    //pega a dezena da centena  (011)
+  num3 = Math.floor((num1 % 10) / 1)    
+} else {
+  console.log(`O número ${numero} não é centena`)
+}
