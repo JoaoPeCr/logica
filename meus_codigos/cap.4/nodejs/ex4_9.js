143001f1b69dd4e01ab013c93e6d27da0100a955
João P. Corrêa <154474733+JoaoPeCr@users.noreply.github.com>
2024-07-29 22:12:09 -0300

    .

--- meus_codigos/cap.4/nodejs/ex4_9.js
+++ meus_codigos/cap.4/nodejs/ex4_9.js
@@ -1,10 +1,12 @@
 const prompt = require("prompt-sync")(); //adiciona o pacote
-const numero = Number(prompt("Número (centena): ")); //recebe a entrada
+const numero = Number(prompt("Número (centena): ")); //recebe a entrada 334
 
-if (numero < 100 || numero >= 1000 ) {      //diz até onde vai o programa
-  num1 = Math.floor(numero / 100)           //pega a unidade da centena (001)
-  num2 = Math.floor((numero % 100) / 10)    //pega a dezena da centena  (011)
-  num3 = Math.floor((num1 % 10) / 1)    
+if (numero > 100 || numero < 1000 ) {      //diz até onde vai o programa
+  num1 = Math.floor(numero / 100)           //pega a unidade da centena (001) 3
+  num2 = Math.floor((numero % 100) / 10)    //pega a dezena da centena  (010) 3
+  num4 = ((numero % 100) / 10)
+  num3 = Math.floor((num4 % 10) / 1)        //pega a centena do centena (100) 4
+  console.log("A centena invérsa é: ")
 } else {
   console.log(`O número ${numero} não é centena`)
 }
