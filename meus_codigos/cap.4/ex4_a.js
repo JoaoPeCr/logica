ce219541eabdfea7b463d640a3c640eb9b9b9483
João P. Corrêa <154474733+JoaoPeCr@users.noreply.github.com>
2024-07-29 23:22:08 -0300

    .

---
+++ meus_codigos/cap.4/ex4_a.js
@@ -0,0 +1,7 @@
+
+const sobra = numero % 2;
+if (sobra > 0) {
+  console.log("Seu Número é ímpar")
+} else {
+ console.log("Seu Número é par");
+}
--- meus_codigos/cap.4/nodejs/ex4_9.js
+++ meus_codigos/cap.4/nodejs/ex4_9.js
@@ -1,12 +1,11 @@
-const prompt = require("prompt-sync")(); //adiciona o pacote
-const numero = Number(prompt("Número (centena): ")); //recebe a entrada 334
-
-if (numero > 100 || numero < 1000 ) {      //diz até onde vai o programa
-  num1 = Math.floor(numero / 100)           //pega a unidade da centena (001) 3
-  num2 = Math.floor((numero % 100) / 10)    //pega a dezena da centena  (010) 3
-  num4 = ((numero % 100) / 10)
-  num3 = Math.floor((num4 % 10) / 1)        //pega a centena do centena (100) 4
-  console.log("A centena invérsa é: ")
-} else {
-  console.log(`O número ${numero} não é centena`)
+const prompt = require("prompt-sync")(); //adiciona o pacote.
+const numero = Number(prompt("Número (centena): ")); //recebe a entrada. 334
+if (numero < 100 || numero >= 1000) {
+  console.log(`Erro.. deve ser uma centena..`); //verifica se o número é aceitável.
+  return;
 }
+const num1 = Math.floor(numero / 100); //pega o número e divide por 100. 3
+const sobra = numero % 100; //pega a sobra da outra operação. 3.34 => 34
+const num2 = Math.floor(sobra / 10) * 10; //divide por dez arredondado e multiplica. 30
+const num3 = Math.floor(sobra % 10) * 100; //pega a sobra da sobra e arredonda e multiplica 400
+console.log(`A centena invérsa é: ${num3 + num2 + num1}`);
